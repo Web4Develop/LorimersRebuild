7b6379204693b5765f77f8322fe915806c9fadaf
Web4Develop <realweb4develop@gmail.com>
2022-03-09 16:31:15 +0000

    Web 4 Develop Lorimers Rebuild v1.0.0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,63 @@
+// the current year
 const d = new Date();
 document.getElementById("date").innerHTML = d.getFullYear();
+
+
+// navigation menu animataion
+function myFunction(menu) {
+  menu.classList.toggle("change");
+}
+
+
+// dropdown menu
+/*
+function myFunction() {
+  document.getElementById("jsDropdown").classList.toggle("jsShow");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".navButton")) {
+    let dropdowns = document.getElementsByClassName("navMenu");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("jsShow")) {
+        openDropdown.classList.remove("jsShow");
+      }
+    }
+  }
+}
+*/
+
+
+// nav menu
+function openNav() {
+  document.getElementById("jsSideNav").style.width = "350px";
+  document.getElementById("jsMain").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("jsSideNav").style.width = "0";
+  document.getElementById("jsMain").style.marginLeft = "0";
+}
+// nav menu
+
+
+// mobile nav menu
+function menuOpen() {
+  document.getElementById("jsNav").style.height = "100%";
+}
+
+function menuClose() {
+  document.getElementById("jsNav").style.height = "0%";
+}
+// / mobile nav menu
+
+
+// don't know
+// document.getElementsByClassName("menu").style.
+// / don't know
+
 
 // const border = "";
 // setInterval(myTimer, 2.5);
